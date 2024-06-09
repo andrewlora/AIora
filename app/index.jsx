ded62c0ef66d11961d604c5b7d73f61b08a99cb1
Andres Lora <jalc.andres.lora@gmail.com>
2024-06-08 21:47:22 -0700

    Add TabLayout with expo-router Tabs component. Created Home, bookmark, create and profile tabs

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ export default function Page() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">AIora</Text>
       <StatusBar style="auto" />
-      <Link href="/profile">Go to Profile</Link>
+      <Link href="/home">Go to Home</Link>
     </View>
   );
 }

58b389be62f742dcbbb843f4280980d841377114
Andres Lora <jalc.andres.lora@gmail.com>
2024-06-08 20:59:30 -0700

    Initial configuration project with expo router and nativewind(tailwind)

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+export default function Page() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">AIora</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile">Go to Profile</Link>
+    </View>
+  );
+}

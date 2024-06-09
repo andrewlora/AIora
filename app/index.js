f10e3cde196f12372beb67b4dc676ce5c20827fe
Andres Lora <jalc.andres.lora@gmail.com>
2024-06-08 21:35:48 -0700

    Add File & Folder Structure

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">AIora</Text>
+      <Text className="text-3xl font-pblack">AIora</Text>
       <StatusBar style="auto" />
       <Link href="/profile">Go to Profile</Link>
     </View>

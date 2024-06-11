8da17090fc0b406d185fd0b0e241ffac98b24b98
Andres Lora <jalc.andres.lora@gmail.com>
2024-06-10 17:01:39 -0700

    Add Profile Screen Component

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -27,7 +27,6 @@ const SignIn = () => {
       const result = await getCurrentUser();
       setUser(result);
       setIsLogged(true);
-      Alert.alert("Success", "User signed in successfully");
       router.replace("/home");
     } catch (error) {
       Alert.alert("Error", error.message);
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -7,10 +7,12 @@ import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import VideoCard from "../../components/VideoCard";
 import { images } from "../../constants";
+import { useGlobalContext } from "../../context/GlobalProvider";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 
 const Home = () => {
+  const { user, getUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
@@ -47,10 +49,10 @@ const Home = () => {
             <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  MarineJS
+                  {user?.username}
                 </Text>
               </View>
 
